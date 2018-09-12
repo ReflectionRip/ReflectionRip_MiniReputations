d89ac7ed4183d713e9820a2b0452cc845dc24054
ReflectionRip <35382665+ReflectionRip@users.noreply.github.com>
2018-09-12 16:18:10 -0400

    Location change of my dll.

--- Release/DeathReputation.cs
+++ Release/DeathReputation.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using XRL.Core;
+using XRL.Messages;
 
 namespace XRL.World.Parts
 {
     [Serializable]
     public class rr_DeathReputation : IPart
     {
-        public int repValue = 1;
+        public float repValue = 1.0f;
+        public float repScale = 30.0f;
+        public float levelScale = 1.0f;
+        public float relationScale = 4.0f;
+        public int gravitateOffset = 300;
+
         public string descriptionPostfix = string.Empty;
         [NonSerialized]
         public List<string> parentFactions = new List<string>();
@@ -219,64 +225,95 @@ namespace XRL.World.Parts
                 // Did the player kill this Creature?
                 if (myKiller.IsPlayer())
                 {
-                    // Figure out how much to adjust by (repValue * Level)
-                    int repMod = repValue * ParentObject.Statistics["Level"].Value;
-                    if (repMod == 0) return true;
-
                     // Adjust the players reputation.
                     Reputation myReputation = XRLCore.Core.Game.PlayerReputation;
                     foreach (KeyValuePair<string, int> item in ParentObject.pBrain.FactionMembership)
                     {
-                        myReputation.modify(item.Key, -repMod, false);
+
+                        // Calculate an adjust 'weight'
+                        // 1) Does the player have a positive or negative relationship to this faction.
+                        //    - Player has a positive relationship to this creature their reputation drops more.
+                        //      (You are friendly they are not going to like that.)
+                        //    - Player has a negitive relationship to this creature their reputation drops less.
+                        //      (You are already hated, they are not going to hate you that much more)
+                        //    - This should be the major factor in the equation.
+                        // 2) Is the creature greater or lesser level than the player.
+                        //    - More difficult creatures offer more reputation.
+                        //    - Easier creatures offer less reputation.
+                        //    - This should be a minor adjustment.
+
+                        // Get the current reputation.
+                        int currentRep = myReputation.get(item.Key);
+                        //MessageQueue.AddPlayerMessage("currentRep " + currentRep);
+
+                        // Scale the reputation based on the current reputation.
+                        float fRep = 0;
+                        //if (currentRep > 0) fRep = repValue * (currentRep / repScale);
+                        //else if (currentRep < 0) fRep = repValue / (-currentRep / repScale);
+                        if (currentRep > -gravitateOffset) fRep = repValue * ((currentRep + gravitateOffset) / repScale);
+                        else if (currentRep < -gravitateOffset) fRep = repValue / (-(currentRep + gravitateOffset) / repScale);
+                        //MessageQueue.AddPlayerMessage("fRep " + fRep);
+
+                        // Get the level differences.
+                        int creatureLevel = ParentObject.Statistics["Level"].Value;
+                        int playerLevel = XRLCore.Core.Game.Player.Body.Statistics["Level"].Value;
+
+                        // Scale the reputation based on the player vs creature levels.
+                        int dLevel = creatureLevel - playerLevel;
+                        if (dLevel > 0) fRep = fRep * (dLevel / levelScale);
+                        else if (dLevel < 0) fRep = fRep / (-dLevel / levelScale);
+                        //MessageQueue.AddPlayerMessage("fRep " + fRep);
+
+                        currentRep = (int)Math.Floor(fRep);
+                        //MessageQueue.AddPlayerMessage("currentRep " + currentRep);
+
+                        // Only modify if the reputation change is not 0.
+                        if (currentRep != 0) myReputation.modify(item.Key, -currentRep, false);
                     }
-                    if ((repMod / 4) > 0)
+
+                    foreach (FriendorFoe relatedFaction in relatedFactions)
                     {
-                        foreach (FriendorFoe relatedFaction in relatedFactions)
+                        // Get the current reputation.
+                        int currentRep = myReputation.get(relatedFaction.faction);
+                        //MessageQueue.AddPlayerMessage("currentRep " + currentRep);
+
+                        // Scale the reputation based on the current reputation.
+                        float fRep = 0;
+                        //if (currentRep > 0) fRep = repValue * (currentRep / repScale);
+                        //else if (currentRep < 0) fRep = repValue / (-currentRep / repScale);
+                        if (currentRep > -gravitateOffset) fRep = repValue * ((currentRep + gravitateOffset) / repScale);
+                        else if (currentRep < -gravitateOffset) fRep = repValue / (-(currentRep + gravitateOffset) / repScale);
+                        //MessageQueue.AddPlayerMessage("fRep " + fRep);
+
+                        // Get the level differences.
+                        int creatureLevel = ParentObject.Statistics["Level"].Value;
+                        int playerLevel = XRLCore.Core.Game.Player.Body.Statistics["Level"].Value;
+
+                        // Scale the reputation based on the player vs creature levels.
+                        int dLevel = creatureLevel - playerLevel;
+                        if (dLevel > 0) fRep = fRep * (dLevel / levelScale);
+                        else if (dLevel < 0) fRep = fRep / (-dLevel / levelScale);
+                        //MessageQueue.AddPlayerMessage("fRep " + fRep);
+
+                        fRep = fRep / relationScale;
+                        currentRep = (int)Math.Floor(fRep);
+                        //MessageQueue.AddPlayerMessage("currentRep " + currentRep);
+
+                        if (relatedFaction.status == "liked")
                         {
-                            if (relatedFaction.status == "liked")
-                            {
-                                myReputation.modify(relatedFaction.faction, -repMod / 4, false);
-                            }
-                            else
-                            {
-                                myReputation.modify(relatedFaction.faction, repMod / 4, false);
-                            }
+                            // Only modify if the reputation change is not 0.
+                            if (currentRep != 0) myReputation.modify(relatedFaction.faction, -currentRep, false);
+                        }
+                        else
+                        {
+                            // Only modify if the reputation change is not 0.
+                            if (currentRep != 0) myReputation.modify(relatedFaction.faction, currentRep, false);
                         }
                     }
                 } 
                 else
                 {
-                    /* This section tries to give the reputation to the killer of the Creature. Normally this is
-                     * not important, but when dominating or when allys make a kill this can be useful to track. 
-                     */
-
-                    // Figure out how much to adjust by repValue
-                    int repMod = repValue;
-                    if (repMod == 0) return true;
-
-                    // Get the brain of the killer.
-                    Brain killersBrain = myKiller.GetPart("Brain") as Brain;
-                    if (killersBrain == null) return true;
-
-                    // Adjust the killers reputation.
-                    foreach (KeyValuePair<string, int> item in ParentObject.pBrain.FactionMembership)
-                    {
-                        AddOrChangeFactionFeelings(killersBrain, item.Key, -repMod);
-                    }
-                    if ((repMod / 2) > 0)
-                    {
-                        foreach (FriendorFoe relatedFaction in relatedFactions)
-                        {
-                            if (relatedFaction.status == "liked")
-                            {
-                                AddOrChangeFactionFeelings(killersBrain, relatedFaction.faction, -repMod / 2);
-                            }
-                            else
-                            {
-                                AddOrChangeFactionFeelings(killersBrain, relatedFaction.faction, repMod / 2);
-                            }
-                        }
-                    }
+                    /* Removed for Now; Where to adjust reputation for creature on creature combat */
                 }
 
                 return true;
